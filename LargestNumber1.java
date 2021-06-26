89480bea00e175dcb7549eece797f6aef8e2d610
Drishti Malhotra <71998960+Drishti05@users.noreply.github.com>
2021-06-26 13:37:43 +0530

    Add files via upload

---
+++ LargestNumber1.java
@@ -0,0 +1,29 @@
+public class LargestNumber1{
+    public static String largestNumber(int nums[]){
+        String largestNumber=new String();
+        for(int index=0;index<nums.length;index++){
+            for(int iterator=index;iterator<nums.length;iterator++){
+                String obj1= Integer.toString(nums[index]);
+                String obj2= Integer.toString(nums[iterator]);
+                String merge1=obj1+obj2;
+                String merge2=obj2+obj1;
+                long num1=Long.parseLong(merge1);
+                long num2=Long.parseLong(merge2);
+                if(num1<num2){
+                    int temporary=nums[index];
+                    nums[index]=nums[iterator];
+                    nums[iterator]=temporary;
+                }
+            }
+        }
+        if(nums[0]==0) return largestNumber="0";
+        for(int index=0;index<nums.length;index++){
+            largestNumber=largestNumber+Integer.toString(nums[index]);
+        }
+        return largestNumber;
+    }
+    public static void main(String[]args){
+        String number=largestNumber(new int[] {23,56,7,9,0});
+        System.out.println(number);
+    }
+}
