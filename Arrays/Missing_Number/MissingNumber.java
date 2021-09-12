0ef4c968ae63629f97b448a6e43c72caf3cc7b18
Drishti Malhotra <71998960+Drishti05@users.noreply.github.com>
2021-09-12 12:22:56 +0530

    Add files via upload

---
+++ Arrays/Missing_Number/MissingNumber.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class MissingNumber {
+    public static int missingNumber(int[] nums) {
+        int actualSum=0;
+        int size=nums.length;
+        int idealSum=(size*(size+1))/2;
+        for(int num:nums){
+            actualSum+=num;
+        }
+        return idealSum-actualSum;
+        
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter the size of the array");
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        Scanner sc1=new Scanner(System.in);
+        int[] arr=new int[size];
+        for(int index=0;index<size;index++){
+            System.out.println("Enter the element");
+            arr[index]=sc1.nextInt();
+        }
+        System.out.println("The Missing number is:"+missingNumber(arr));
+
+
+        
+    }
+    
+}
