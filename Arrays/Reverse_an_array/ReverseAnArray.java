2283368bb24d65f42fc552bc1afdec18f7166bac
Drishti Malhotra <71998960+Drishti05@users.noreply.github.com>
2021-10-02 13:55:03 +0530

    Update ReverseAnArray.java

--- Arrays/Reverse_an_array/ReverseAnArray.java
+++ Arrays/Reverse_an_array/ReverseAnArray.java
@@ -1,1 +1,38 @@
+import java.util.Scanner;
+public class ReverseAnArray{
+    public static void reverse(int[] arr){
+        int size=arr.length;
+        int end=size-1;
+        for(int start=0;start<size/2;start++,end--){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+        }
+    }
 
+    public static void print(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter the size of array");
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        Scanner sc1=new Scanner(System.in);
+        int[] arr=new int[size];
+        for(int i=0;i<size;i++){
+            System.out.println("Enter the Element");
+            arr[i]=sc1.nextInt();
+        }
+        System.out.println("The original array:");
+        print(arr);
+        reverse(arr);
+        System.out.println();
+        System.out.println("The reversed array:");
+        print(arr);
+
+    }
+    
+}
