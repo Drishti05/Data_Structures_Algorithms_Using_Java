515630982432dfc60ba45e001b75ebff255755bf
Drishti Malhotra <71998960+Drishti05@users.noreply.github.com>
2021-10-03 18:49:55 +0530

    Update RemoveDuplicatesFromSoretedArray2.java

--- Arrays/Remove_Duplicates_From_Sorted_Array/RemoveDuplicatesFromSoretedArray2.java
+++ Arrays/Remove_Duplicates_From_Sorted_Array/RemoveDuplicatesFromSoretedArray2.java
@@ -1,1 +1,31 @@
+import java.util.Scanner;
+public class RemoveDuplicatesFromSortedArray {
+    public static int removeDuplicates(int[] nums) {
+        int size=nums.length;
+        int j=0;
+        if(size==0 || size==1) return size;
+        for(int i=0;i<size-1;i++){
+            if(nums[i]!=nums[i+1]){
+                nums[j++]=nums[i];
+            }
+        }
+        nums[j++]=nums[size-1];
+        return j;
+    }
+            public static void main(String[] args) {
+        System.out.println("Enter the size of array");
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        Scanner sc1=new Scanner(System.in);
+        int[] nums=new int[size];
+        for(int i=0;i<size;i++){
+            System.out.println("Enter the element");
+            nums[i]=sc1.nextInt();
+        }
+        int answer=removeDuplicates(nums);
+        System.out.println("The answer is:"+answer);
+        
+    }
+    
+}
 
